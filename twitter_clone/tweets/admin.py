5296e5495143dd0fbdb4b75dd41d4f8e0e9fb909
George Hlongwane <georgehlongwane8@gmail.com>
2024-09-19 15:58:04 +0200

    Created rules for editing tweets

--- twitter_clone/tweets/admin.py
+++ twitter_clone/tweets/admin.py
@@ -1,3 +1,5 @@
+# The admin is where we will write our first Tweet
 from django.contrib import admin
+from .models import Tweet
 
-# Register your models here.
+admin.site.register(Tweet) # we will have access to the data specified in our models file
